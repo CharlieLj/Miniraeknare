9c4e17f8fc53de5d950d90b86e4466e44d3694e7
CharlieLj <charlie.ljungberg.webd23jon@edu.tucsweden.se>
2023-12-19 16:12:10 +0100

    lista för historik

--- Program.cs
+++ Program.cs
@@ -6,5 +6,7 @@ class Miniräknare
     static void Main()
     {
         Console.WriteLine("Välkommen till Miniräknaren!");
+        // skapar en lista (resultatHistorik) för att spara historiken över tidigare resultat.
+        List<double> resultatHistorik = new List<double>();
     }
 }
